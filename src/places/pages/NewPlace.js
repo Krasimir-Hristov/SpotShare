a1e2bfeb5fabcb747ee561db0369526b282603d2
krasi <krasimir.xristov@gmail.com>
2023-05-23 18:28:49 +0200

    Manage state in the input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,13 @@ import './NewPlace.css';
 export default function NewPlace() {
   return (
     <form className='place-form'>
-      <Input element='input' type='text' label='Title' />
+      <Input
+        element='input'
+        type='text'
+        label='Title'
+        validators={[]}
+        errorText='Please enter a valid title.'
+      />
     </form>
   );
 };
