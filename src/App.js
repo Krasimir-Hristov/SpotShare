15563bb2f0c8ad72b3a8062ca04a3384c86404e2
krasi <krasimir.xristov@gmail.com>
2023-05-23 20:05:14 +0200

    starting the 'Update Place' page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -27,6 +28,9 @@ const App = () => {
         </Route>
         <Route path="/places/new" exact>
           <NewPlace />
+        </Route>
+        <Route path='/places/placeId'>
+          <UpdatePlace />
         </Route>
         <Redirect to="/" />
       </Switch>
