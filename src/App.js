db09e5529774d34d58853e9b72a57f7bd4d26a7c
krasi <krasimir.xristov@gmail.com>
2023-05-23 21:04:02 +0200

    Some bug fixes on input component and PlaceItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
@@ -7,37 +12,39 @@ import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
+
+
 const App = () => {
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
     script.async = true;
     document.body.appendChild(script);
   }, []);
-
+  
   return (
     <Router>
       <MainNavigation />
-      <main> 
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path='/:userId/places' exact>
-          <UserPlaces />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Route path='/places/placeId'>
-          <UpdatePlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
     </Router>
   );
 };
 
 export default App;
-
--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -40,7 +40,7 @@ export default function PlaceItem(props) {
                     </div>
                     <div className='place-item__actions'>
                         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-                        <Button to={`places/${props.id}`}>EDIT</Button>
+                        <Button to={`/places/${props.id}`}>EDIT</Button>
                         <Button danger>DELETE</Button>
                     </div>
                 </Card>
