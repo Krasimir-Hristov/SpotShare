34d0161a49b9b702a1f2ab42122cfcc8b1a5dda3
krasi <krasimir.xristov@gmail.com>
2023-05-28 20:53:52 +0200

    adding singup form

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import { useForm } from '../../shared/hooks/form-hook';
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './Auth.css';
 
 export default function Auth() {
+
+    const [isLoginMode, setIsLoginMode] = useState(true);
 
     const [formState, inputHandler] = useForm({
         email: {
@@ -20,6 +22,10 @@ export default function Auth() {
         }
     }, false);
 
+    const switchModeHandler = () => {
+        setIsLoginMode(prevMode => !prevMode);
+    };
+
     const authSubmitHandler = event => {
         event.preventDefault();
         console.log(formState.inputs);
@@ -30,6 +36,17 @@ export default function Auth() {
             <h2>Login Required</h2>
             <hr />
             <form onSubmit={authSubmitHandler}>
+                {!isLoginMode && (
+                    <Input
+                        element='input'
+                        id='name'
+                        type='text'
+                        label='Your Name'
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText='Please eneter a name.'
+                        onInput={inputHandler}
+                    />
+                )}
                 <Input
                     element='input'
                     id='email'
@@ -50,9 +67,14 @@ export default function Auth() {
                     onInput={inputHandler}
                 />
 
-                <Button type='submit' disabled={!formState.isValid}>LOGIN</Button>
+                <Button type='submit' disabled={!formState.isValid}>
+                    {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+                </Button>
 
             </form>
+            <Button inverse onClick={switchModeHandler} >
+                SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+            </Button>
         </Card>
     )
 }
