41c9bd74f15ffa7e1e0d7829d18b4d9dfcec5511
krasi <krasimir.xristov@gmail.com>
2023-06-03 15:08:56 +0200

    Finish my errorHandling for singup

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -78,6 +78,10 @@ export default function Auth() {
                 });
 
                 const responseData = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
                 console.log(responseData);
 
                 setIsLoading(false);
@@ -94,52 +98,59 @@ export default function Auth() {
 
     };
 
+    const errorHandler = () => {
+        setError(null);
+    };
+
     return (
-        <Card className='authentication'>
-            {isLoading && <LoadingSpinner asOverlay />}
-            <h2>Login Required</h2>
-            <hr />
-            <form onSubmit={authSubmitHandler}>
-                {!isLoginMode && (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler}/>
+            <Card className='authentication'>
+                {isLoading && <LoadingSpinner asOverlay />}
+                <h2>Login Required</h2>
+                <hr />
+                <form onSubmit={authSubmitHandler}>
+                    {!isLoginMode && (
+                        <Input
+                            element='input'
+                            id='name'
+                            type='text'
+                            label='Your Name'
+                            validators={[VALIDATOR_REQUIRE()]}
+                            errorText='Please eneter a name.'
+                            onInput={inputHandler}
+                        />
+
+                    )}
                     <Input
                         element='input'
-                        id='name'
-                        type='text'
-                        label='Your Name'
-                        validators={[VALIDATOR_REQUIRE()]}
-                        errorText='Please eneter a name.'
+                        id='email'
+                        type='email'
+                        label='E-Mail'
+                        validators={[VALIDATOR_EMAIL()]}
+                        errorText='Please enter a valid email address.'
                         onInput={inputHandler}
                     />
 
-                )}
-                <Input
-                    element='input'
-                    id='email'
-                    type='email'
-                    label='E-Mail'
-                    validators={[VALIDATOR_EMAIL()]}
-                    errorText='Please enter a valid email address.'
-                    onInput={inputHandler}
-                />
+                    <Input
+                        element='input'
+                        id='password'
+                        type='password'
+                        label='Password'
+                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        errorText='Please enter a valid password, at least 5 characters.'
+                        onInput={inputHandler}
+                    />
 
-                <Input
-                    element='input'
-                    id='password'
-                    type='password'
-                    label='Password'
-                    validators={[VALIDATOR_MINLENGTH(5)]}
-                    errorText='Please enter a valid password, at least 5 characters.'
-                    onInput={inputHandler}
-                />
+                    <Button type='submit' disabled={!formState.isValid}>
+                        {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+                    </Button>
 
-                <Button type='submit' disabled={!formState.isValid}>
-                    {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+                </form>
+                <Button inverse onClick={switchModeHandler} >
+                    SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
                 </Button>
-
-            </form>
-            <Button inverse onClick={switchModeHandler} >
-                SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
-            </Button>
-        </Card>
+            </Card>
+        </React.Fragment>
     )
 }
