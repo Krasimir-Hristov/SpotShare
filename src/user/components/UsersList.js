4aa64b3d0180fdca5a37b26936337e6162a7ed3e
krasi <krasimir.xristov@gmail.com>
2023-06-03 17:29:32 +0200

    i make some great changes bu debug my app

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,32 +1,34 @@
 import React from 'react';
 
-import UsersItem from './UserItem';
+import UserItem from './UserItem';
 import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css';
 
-export default function UsersList( props ) {
+const UsersList = props => {
 
-    if (props.items.length === 0) {
-        return (
-            <div className='center'>
-                <Card> 
-                <h2>No users found.</h2>
-                </Card> 
-            </div>
-          );
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
+      </div>
+    );
+  }
 
-    };
-
-    return <ul className='users-list'> 
-        {props.items.map( user => ( 
-             <UsersItem 
-            key={ user.id } 
-            id={ user.id } 
-            image={ user.image } 
-            name={ user.name } 
-            placeCount={ user.places } 
-            />
-        ))};
-    </ul>;
-  
+  return (
+    <ul className="users-list">
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places.length}
+        />
+      ))}
+    </ul>
+  );
 };
+
+export default UsersList;
