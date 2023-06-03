31a6458f611b70ea1dc8a36d725f42f7030451df
krasi <krasimir.xristov@gmail.com>
2023-06-03 20:04:36 +0200

    create custom hook for my http requests

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,67 @@
+import { useCallback, useState, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(undefined);
+
+    const activeHttpRequests = useRef([]);
+
+    const sendRequest = useCallback(async (
+        url,
+        method = 'GET',
+        body = null,
+        headers = {}
+    ) => {
+
+        setIsLoading(true);
+
+        const httpAbortController = new AbortController();
+        activeHttpRequests.current.push(httpAbortController);
+
+        try {
+
+            const response = await fetch(url, {
+                method,
+                body,
+                headers,
+                signal: httpAbortController.signal
+            });
+
+            const responseData = await response.json();
+
+            activeHttpRequests.current = activeHttpRequests.current.filter(
+                reqController => reqController !== httpAbortController
+            );
+
+            if (!response.ok) {
+                throw new Error(responseData.message);
+            }
+
+            setIsLoading(false);
+
+            return responseData;
+        } catch (err) {
+
+            setError(err.message);
+            setIsLoading(false);
+            throw err;
+        }
+
+    }, []);
+
+    const clearError = () => {
+
+        setError(null);
+    };
+
+    useEffect(() => {
+
+        return () => {
+
+            activeHttpRequests.current.forEach(abortController => abortController.abort());
+        };
+    }, []);
+
+    return { isLoading, error, sendRequest, clearError }
+};
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -6,6 +6,7 @@ import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { useForm } from '../../shared/hooks/form-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './Auth.css';
@@ -14,8 +15,7 @@ export default function Auth() {
 
     const auth = useContext(AuthContext);
     const [isLoginMode, setIsLoginMode] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(undefined);
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -56,84 +56,51 @@ export default function Auth() {
     const authSubmitHandler = async event => {
         event.preventDefault();
 
-        setIsLoading(true);
 
         if (isLoginMode) {
 
             try {
 
-
-                const response = await fetch('http://localhost:5000/api/users/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
+                await sendRequest(
+                    'http://localhost:5000/api/users/login',
+                    'POST',
+                    JSON.stringify({
                         email: formState.inputs.email.value,
                         password: formState.inputs.password.value
-                    })
-                });
-
-                const responseData = await response.json();
-
-                if (!response.ok) {
-                    throw new Error(responseData.message);
-                }
-
-                setIsLoading(false);
+                    }),
+                    {
+                        'Content-Type': 'application/json'
+                    }
+                );
 
                 auth.login();
-            } catch (err) {
+            } catch (err) { };
 
-                console.log(err);
-                setIsLoading(false);
-
-                setError(err.message || 'Somthing went wrong, please try again later.');
-            }
         } else {
 
             try {
 
-
-                const response = await fetch('http://localhost:5000/api/users/signup', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
+                await sendRequest(
+                    'http://localhost:5000/api/users/signup',
+                    'POST',
+                    JSON.stringify({
                         name: formState.inputs.name.value,
                         email: formState.inputs.email.value,
                         password: formState.inputs.password.value
-                    })
-                });
-
-                const responseData = await response.json();
-
-                if (!response.ok) {
-                    throw new Error(responseData.message);
-                }
-
-                setIsLoading(false);
+                    }),
+                    {
+                        'Content-Type': 'application/json'
+                    },
+                );
 
                 auth.login();
-            } catch (err) {
-
-                console.log(err);
-                setIsLoading(false);
-
-                setError(err.message || 'Somthing went wrong, please try again later.');
-            }
+            } catch (err) { };
         };
-
-    };
-
-    const errorHandler = () => {
-        setError(null);
     };
 
     return (
         <React.Fragment>
-            <ErrorModal error={error} onClear={errorHandler} />
+            <ErrorModal error={error} onClear={clearError} />
             <Card className='authentication'>
                 {isLoading && <LoadingSpinner asOverlay />}
                 <h2>Login Required</h2>
@@ -166,7 +133,7 @@ export default function Auth() {
                         id='password'
                         type='password'
                         label='Password'
-                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        validators={[VALIDATOR_MINLENGTH(6)]}
                         errorText='Please enter a valid password, at least 5 characters.'
                         onInput={inputHandler}
                     />
