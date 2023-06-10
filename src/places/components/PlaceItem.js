83a0bc7afa46562d43bfc39fbf9b36641bbea54c
krasi <krasimir.xristov@gmail.com>
2023-06-10 22:47:10 +0200

    Update

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -10,91 +10,107 @@ import { AuthContext } from '../../shared/context/auth-context';
 import { useHttpClient } from '../../shared/hooks/http-hook';
 import './PlaceItem.css';
 
-export default function PlaceItem(props) {
+const PlaceItem = props => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const auth = useContext(AuthContext);
+  const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
-    const auth = useContext(AuthContext);
+  const openMapHandler = () => setShowMap(true);
 
-    const { isLoading, error, sendRequest, clearError } = useHttpClient();
-    const [showMap, setShowMap] = useState(false);
-    const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const closeMapHandler = () => setShowMap(false);
 
-    const openMapHandler = () => setShowMap(true);
-    const closeMapHandler = () => setShowMap(false);
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
 
-    const showDeleteWatningHandler = () => {
-        setShowConfirmModal(true);
-    };
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+  };
 
-    const cancelDeleteHandler = () => {
-        setShowConfirmModal(false);
-    };
+  const confirmDeleteHandler = async () => {
+    setShowConfirmModal(false);
+    try {
+      await sendRequest(
+        `http://localhost:5000/api/places/${props.id}`,
+        'DELETE',
+        null,
+        {
+          Authorization: 'Bearer ' + auth.token
+        }
+      );
+      props.onDelete(props.id);
+    } catch (err) {}
+  };
 
-    const confirmDeleteHandler = async () => {
-        setShowConfirmModal(false);
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
+        </div>
+      </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this place? Please note that it
+          can't be undone thereafter.
+        </p>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          {isLoading && <LoadingSpinner asOverlay />}
+          <div className="place-item__image">
+            <img
+              src={`http://localhost:5000/${props.image}`}
+              alt={props.title}
+            />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
+            {auth.userId === props.creatorId && (
+              <Button to={`/places/${props.id}`}>EDIT</Button>
+            )}
 
-        try {
-
-            await sendRequest(
-                `http://localhost:5000/api/places/${props.id}`,
-                'DELETE'
-            );
-
-            props.onDelete(props.id);
-        } catch (err) { };
-
-    };
-
-    return (
-        <React.Fragment>
-            <ErrorModal error={error} onClear={clearError} />
-            <Modal
-                show={showMap}
-                onCancel={closeMapHandler}
-                header={props.address}
-                contentClass='plate-item__modal-content'
-                footerClass='place-item__modal-actions'
-                footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
-            >
-                <div className='map-container'>
-                    <Map center={props.coordinates} zoom={16} />
-                </div>
-            </Modal>
-
-            <Modal
-                show={showConfirmModal}
-                onCancel={cancelDeleteHandler}
-                header='Are you sure?'
-                footerClass='place-item__modal-actions'
-                footer={
-                    <React.Fragment>
-                        <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
-                        <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
-                    </React.Fragment>
-                }>
-                <p>Do you want to proceed and delete this place? Please note that can't be undone thereafter.</p>
-            </Modal>
-            <li className='place-item'>
-                <Card className='place-item__content'>
-                    {isLoading && <LoadingSpinner asOverlay />}
-                    <div className='place-item__image'>
-                        <img src={`http://localhost:5000/${props.image}`} alt={props.title} />
-                    </div>
-                    <div className='place-item__info'>
-                        <h2>{props.title}</h2>
-                        <h3>{props.address}</h3>
-                        <p>{props.description}</p>
-                    </div>
-                    <div className='place-item__actions'>
-                        <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-
-                        {auth.userId === props.creatorId &&
-                            (<Button to={`/places/${props.id}`}>EDIT</Button>)}
-
-                        {auth.userId === props.creatorId &&
-                            (<Button danger onClick={showDeleteWatningHandler}>DELETE</Button>)}
-                    </div>
-                </Card>
-            </li>
-        </React.Fragment>
-    );
+            {auth.userId === props.creatorId && (
+              <Button danger onClick={showDeleteWarningHandler}>
+                DELETE
+              </Button>
+            )}
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
+  );
 };
+
+export default PlaceItem;
--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -15,91 +15,80 @@ import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
 import './PlaceForm.css';
 
-
-
-
 const UpdatePlace = () => {
-
   const auth = useContext(AuthContext);
-
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const [loadedPlaces, setLoadedPlaces] = useState(undefined);
-
-
+  const [loadedPlace, setLoadedPlace] = useState();
   const placeId = useParams().placeId;
   const history = useHistory();
 
-  const [formState, inputHandler, setFormData] = useForm({
-    title: {
-      value: '',
-      isValid: false,
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      }
     },
-    description: {
-      value: '',
-      isValid: false
-    }
-  }, false
+    false
   );
 
   useEffect(() => {
-
-    const fetchPlaces = async () => {
-
+    const fetchPlace = async () => {
       try {
-
         const responseData = await sendRequest(
           `http://localhost:5000/api/places/${placeId}`
         );
-
-        setLoadedPlaces(responseData.place);
-
-        setFormData({
-          title: {
-            value: responseData.place.title,
-            isValid: true,
+        setLoadedPlace(responseData.place);
+        setFormData(
+          {
+            title: {
+              value: responseData.place.title,
+              isValid: true
+            },
+            description: {
+              value: responseData.place.description,
+              isValid: true
+            }
           },
-          description: {
-            value: responseData.place.description,
-            isValid: true
-          }
-        }, true);
-      } catch (err) { };
-
+          true
+        );
+      } catch (err) {}
     };
-    fetchPlaces();
+    fetchPlace();
   }, [sendRequest, placeId, setFormData]);
 
   const placeUpdateSubmitHandler = async event => {
-
     event.preventDefault();
-
     try {
-
       await sendRequest(
         `http://localhost:5000/api/places/${placeId}`,
         'PATCH',
         JSON.stringify({
           title: formState.inputs.title.value,
-          description: formState.inputs.description.value,
+          description: formState.inputs.description.value
         }),
-        { 'Content-Type': 'application/json' }
+        {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + auth.token
+        }
       );
-
       history.push('/' + auth.userId + '/places');
-    } catch (err) { };
+    } catch (err) {}
   };
 
   if (isLoading) {
-
     return (
-      <div className='center'>
+      <div className="center">
         <LoadingSpinner />
       </div>
     );
   }
 
-  if (!loadedPlaces && !error) {
-
+  if (!loadedPlace && !error) {
     return (
       <div className="center">
         <Card>
@@ -110,36 +99,36 @@ const UpdatePlace = () => {
   }
 
   return (
-
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
-      {isLoading && (<LoadingSpinner />)}
-      {!isLoading && loadedPlaces && <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
-        <Input
-          id="title"
-          element="input"
-          type="text"
-          label="Title"
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter a valid title."
-          onInput={inputHandler}
-          initialValue={loadedPlaces.title}
-          initialValid={true}
-        />
-        <Input
-          id="description"
-          element="textarea"
-          label="Description"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid description (min. 5 characters)."
-          onInput={inputHandler}
-          initialValue={loadedPlaces.description}
-          initialValid={true}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          UPDATE PLACE
-        </Button>
-      </form>}
+      {!isLoading && loadedPlace && (
+        <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+          <Input
+            id="title"
+            element="input"
+            type="text"
+            label="Title"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid title."
+            onInput={inputHandler}
+            initialValue={loadedPlace.title}
+            initialValid={true}
+          />
+          <Input
+            id="description"
+            element="textarea"
+            label="Description"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid description (min. 5 characters)."
+            onInput={inputHandler}
+            initialValue={loadedPlace.description}
+            initialValid={true}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            UPDATE PLACE
+          </Button>
+        </form>
+      )}
     </React.Fragment>
   );
 };
