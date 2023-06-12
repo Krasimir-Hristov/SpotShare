7f75e0dac749199835ce247462bb4e777e2f27f2
krasi <krasimir.xristov@gmail.com>
2023-06-12 15:35:20 +0200

    Manage my expiration time

--- src/App.js
+++ src/App.js
@@ -15,11 +15,9 @@ import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
 
+let logoutTimer = undefined;
 
 const App = () => {
-
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(null);
 
   useEffect(() => {
     const script = document.createElement('script');
@@ -28,28 +26,62 @@ const App = () => {
     document.body.appendChild(script);
   }, []);
 
-  const login = useCallback((userId, token) => {
+  const [token, setToken] = useState(false);
+  const [userId, setUserId] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState(undefined);
+
+  const login = useCallback((userId, token, expirationDate) => {
 
     setToken(token);
     setUserId(userId);
-    localStorage.setItem('userData', JSON.stringify({ userId: userId, token: token }));
+
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+
+    setTokenExpirationDate(tokenExpirationDate);
+
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: userId,
+        token: token,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
 
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
-    
+
     localStorage.removeItem('userData');
   }, []);
 
   useEffect(() => {
 
+    if (token && tokenExpirationDate) {
+
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+
     const storedData = JSON.parse(localStorage.getItem('userData'));
 
-    if (storedData && storedData.token) {
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()) {
 
-      login(storedData.userId, storedData.token);
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
     }
   }, [login]);
 
